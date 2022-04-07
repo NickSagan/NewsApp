b8f02617a208f740fa26d5a1ffb8d4346d886abb
NickSagan <9952728@gmail.com>
2022-04-07 10:30:54 +0300

    add func addShadow()

--- NewsApp/NewsCell.swift
+++ NewsApp/NewsCell.swift
@@ -145,5 +145,13 @@ class NewsCell: UICollectionViewCell {
         }
     }
     
-
+    func addShadow() {
+        self.clipsToBounds = false
+        self.backgroundColor = .systemBackground
+        self.layer.cornerRadius = 10
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0.0)
+        self.layer.shadowRadius = 10
+        self.layer.shadowOpacity = 0.2
+    }
 }
