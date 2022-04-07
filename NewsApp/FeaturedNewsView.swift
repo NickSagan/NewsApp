44412233d7faf7b26c034c245fdc3cce34cf45fb
NickSagan <9952728@gmail.com>
2022-04-07 11:18:30 +0300

    add subviews

--- NewsApp/FeaturedNewsView.swift
+++ NewsApp/FeaturedNewsView.swift
@@ -10,4 +10,72 @@ import SnapKit
 
 class FeaturedNewsView: UIView {
     
+    let newsImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .systemBlue // ONLY FOR TEST
+        return imageView
+    }()
+    
+    let newsTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Verdana", size: 24)
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.textColor = .white
+        label.numberOfLines = 2
+        label.text = "This is a news title label This is a news title label" // ONLY FOR TEST
+        return label
+    }()
+    
+    let newsDate: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Verdana", size: 18)
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = .white
+        label.text = "April 07, 2022" // ONLY FOR TEST
+        return label
+    }()
+    
+    let newsComments: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Verdana", size: 18)
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = .white
+        label.text = "cmnts: 0" // ONLY FOR TEST
+        return label
+    }()
+    
+    let newsAgency: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Verdana", size: 18)
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = .white
+        label.text = "CNN" // ONLY FOR TEST
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        addSubview(newsImage)
+        newsImage.addSubview(newsTitle)
+        newsImage.addSubview(newsDate)
+        newsImage.addSubview(newsComments)
+        newsImage.addSubview(newsAgency)
+        
+
+        newsImage.translatesAutoresizingMaskIntoConstraints = false
+        newsTitle.translatesAutoresizingMaskIntoConstraints = false
+        newsDate.translatesAutoresizingMaskIntoConstraints = false
+        newsComments.translatesAutoresizingMaskIntoConstraints = false
+        newsAgency.translatesAutoresizingMaskIntoConstraints = false
+
+    }
 }
