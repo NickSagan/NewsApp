4a9e1f3fa06cb86e8ba9746b03700e9fa23f1d3f
NickSagan <9952728@gmail.com>
2022-04-07 14:18:16 +0300

    create struct NewsFeedManager

---
+++ NewsApp/Models/NewsFeedManager.swift
@@ -0,0 +1,16 @@
+//
+//  NewsFeedManager.swift
+//  NewsApp
+//
+//  Created by Nick Sagan on 07.04.2022.
+//
+
+import Foundation
+
+struct NewsFeedManager {
+    private let apiKey = "3694dad9cbc345c3b86afe90fe329cca"
+    private let featuredNewsUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey="
+    private let otherNewsUrl = "https://newsapi.org/v2/everything?domains=techcrunch.com&sortBy=popularity&apiKey="
+    
+
+}
