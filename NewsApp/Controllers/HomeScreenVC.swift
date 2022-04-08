b98d09d4ffe5489adf4d655450540303f0e46011
NickSagan <9952728@gmail.com>
2022-04-08 16:54:57 +0300

    refactor HomeScreen

--- NewsApp/Controllers/HomeScreenVC.swift
+++ NewsApp/Controllers/HomeScreenVC.swift
@@ -15,16 +15,13 @@ class HomeScreenVC: UIViewController {
     private var refreshControl: UIRefreshControl!
     
     var newsFeedManager = NewsFeedManager()
-    var featuredNews: [News] = []
-    var otherNews: [News] = []
-    
     var cache = NSCache<NSString, UIImage>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         newsFeedManager.delegate = self
         newsFeedManager.fetchFeaturedNews()
-        newsFeedManager.fetchOtherNews()
+        newsFeedManager.fetchOtherNews(page: 1)
         addHeaderView()
         addNewsFeedView()
         setConstraints()
@@ -93,8 +90,9 @@ class HomeScreenVC: UIViewController {
     }
     
     @objc func refresh(_ sender: AnyObject) {
-        newsFeedManager.fetchFeaturedNews()
-        newsFeedManager.fetchOtherNews()
+        Shared.instance.page = 1
+        newsFeedManager.fetchFeaturedNews(refresh: true)
+        newsFeedManager.fetchOtherNews(page: 1, refresh: true)
         usleep(UInt32(0.5))
         refreshControl.endRefreshing()
     }
@@ -105,11 +103,12 @@ class HomeScreenVC: UIViewController {
 extension HomeScreenVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return otherNews.count
+        return Shared.instance.otherNews.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewsCell
+        let otherNews = Shared.instance.otherNews
         cell.newsTitle.text = otherNews[indexPath.row].title
         cell.newsDescription.text = otherNews[indexPath.row].description
         cell.newsDate.text = otherNews[indexPath.row].publishedAt
@@ -132,8 +131,17 @@ extension HomeScreenVC: UICollectionViewDataSource, UICollectionViewDelegateFlow
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard !Shared.instance.otherNews.isEmpty else { return }
+        if indexPath.row > Shared.instance.otherNews.count - 10 {
+            Shared.instance.page += 1
+            newsFeedManager.fetchOtherNews(page: Shared.instance.page)
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as! FeaturedNewsView
+        let featuredNews = Shared.instance.featuredNews
         guard !featuredNews.isEmpty else { return header }
         header.newsTitle.text = featuredNews[0].title
         header.newsDate.text = featuredNews[0].publishedAt
@@ -161,6 +169,7 @@ extension HomeScreenVC: UICollectionViewDataSource, UICollectionViewDelegateFlow
         return header
     }
     
+    // HEADER size
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: collectionView.frame.width * 0.65)
     }
@@ -171,14 +180,14 @@ extension HomeScreenVC: UICollectionViewDataSource, UICollectionViewDelegateFlow
 extension HomeScreenVC: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let url = URL(string: otherNews[indexPath.row].url) else { return }
+        guard let url = URL(string: Shared.instance.otherNews[indexPath.row].url) else { return }
         let vc = WebViewVC()
         vc.selectedSite = url
         navigationController?.pushViewController(vc, animated: true)
     }
     
     @objc private func tapHeader(sender: UITapGestureRecognizer) {
-        guard let url = URL(string: featuredNews[0].url) else { return }
+        guard let url = URL(string: Shared.instance.featuredNews[0].url) else { return }
         let vc = WebViewVC()
         vc.selectedSite = url
         navigationController?.pushViewController(vc, animated: true)
@@ -188,19 +197,16 @@ extension HomeScreenVC: UICollectionViewDelegate {
 //MARK: - NewsFeedManagerDelegate
 
 extension HomeScreenVC: NewsFeedManagerDelegate {
+    
     func didRecieveFeaturedNews(_ newsFeedManager: NewsFeedManager, news: [News]) {
         DispatchQueue.main.async {
-            self.featuredNews += news
             self.collectionView.reloadData()
-            print("Featured news: count = \(self.featuredNews.count)")
         }
     }
     
     func didRecieveOtherNews(_ newsFeedManager: NewsFeedManager, news: [News]) {
         DispatchQueue.main.async {
-            self.otherNews += news
             self.collectionView.reloadData()
-            print("Other news: count = \(self.otherNews.count)")
         }
     }
 }
